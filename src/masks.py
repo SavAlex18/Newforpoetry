c0fd2a9f75b58ce4f800f8e89f3a21449b34caa3
AlexSavchuk <saaliv18@mail.ru>
2026-09-12 22:11:34 +0500

    Initial commit

---
+++ src/masks.py
@@ -0,0 +1,22 @@
+def get_mask_card_number(card_number: str) -> str | None:
+    """Маскирует номер банковской карты, оставляя 6 первых и 4 последних цифры"""
+    if len(card_number) != 16:
+        return None
+    res = [card_number[i : i + 4] for i in range(0, len(card_number), 4)]
+    mask = []
+    for index, item in enumerate(res):
+        if index in (0, 3):
+            mask.append(item)
+        elif index == 1:
+            mask.append(item[0:2] + 2 * "*")
+        else:
+            mask.append(4 * "*")
+    mask_number = " ".join(mask)
+    return mask_number
+
+
+def get_mask_account(account_number: str) -> str | None:
+    if len(account_number) < 4:
+        return None
+    """Маскирует счёт клиента оставляя последние 4 цифры"""
+    return 2 * "*" + account_number[-4:]
