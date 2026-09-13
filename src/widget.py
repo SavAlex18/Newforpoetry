51dbcdb542640970b461b44b5a2001337ead7d90
AlexSavchuk <saaliv18@mail.ru>
2026-09-13 22:38:47 +0500

    ''

--- src/widget.py
+++ src/widget.py
@@ -6,7 +6,6 @@ import masks
 def mask_account_card(info_client: str) -> str | None:
     """Функция возвращает замаскированный номер или счёт в зависимости от входного параметра"""
     info_card_account = info_client.split(" ")
-    # print(info_card_account, info_card_account[0].upper(), info_card_account[0 : len(info_card_account) - 1])
     if info_card_account[0].upper() in ["СЧЕТ", "СЧЁТ"]:
         mask_account = masks.get_mask_account(info_card_account[-1])
         if mask_account:
@@ -23,4 +22,5 @@ def mask_account_card(info_client: str) -> str | None:
 
 
 def get_date(input_datetime: str) -> str:
+    """Функция возвращает 'немецкий' формат даты в виде дд.мм.гггг из ISO формата"""
     return datetime.datetime.fromisoformat(input_datetime).strftime("%d.%m.%Y")
