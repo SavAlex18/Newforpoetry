2283cfdd6fcb60327aaf6f7fbec3f2bf94adb08c
AlexSavchuk <saaliv18@mail.ru>
2026-09-20 20:00:17 +0500

    Added empty modul processing

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+from blib2to3.pytree import Any
+
+
+def filter_by_state(in_list_operation: list[dict[str,Any]], concept_str:str='EXECUTED')-> list[dict[str, Any]]:
+    pass
+
+def sort_by_date(in_list_operation: list[dict[str, Any]], order_by:bool=False) -> list[dict[str, Any]]:
+    pass
