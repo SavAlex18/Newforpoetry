8b6efbd982769ea0b92b6b6e5a3ad838c71a53c6
AlexSavchuk <saaliv18@mail.ru>
2026-09-20 20:53:00 +0500

    Wrote functions filter_by_state and sort_by_date in module processing

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,14 @@
-from blib2to3.pytree import Any
+from datetime import datetime
+from typing import Any
 
 
 def filter_by_state(in_list_operation: list[dict[str,Any]], concept_str:str='EXECUTED')-> list[dict[str, Any]]:
-    pass
+    """ функция возвращает новый список по ключу concept_str, по умолчанию в статусе 'EXECUTED' """
+    return [operation for operation in in_list_operation if operation.get('state','') == concept_str]
 
-def sort_by_date(in_list_operation: list[dict[str, Any]], order_by:bool=False) -> list[dict[str, Any]]:
-    pass
+
+def sort_by_date(in_list_operation: list[dict[str, Any]], order_by:bool=True) -> list[dict[str, Any]]:
+    """Функция возвращает отсортированный по дате операции, по умолчанию - по убыванию"""
+    return sorted(in_list_operation, key=lambda rec: datetime.fromisoformat(rec['date']), reverse=order_by)
+
+
