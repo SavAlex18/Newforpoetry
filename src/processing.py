a0b70bf3771e568503357643a00d6e7b5bac204d
AlexSavchuk <saaliv18@mail.ru>
2026-09-20 21:18:51 +0500

    Processing

--- src/processing.py
+++ src/processing.py
@@ -2,13 +2,11 @@ from datetime import datetime
 from typing import Any
 
 
-def filter_by_state(in_list_operation: list[dict[str,Any]], concept_str:str='EXECUTED')-> list[dict[str, Any]]:
-    """ функция возвращает новый список по ключу concept_str, по умолчанию в статусе 'EXECUTED' """
-    return [operation for operation in in_list_operation if operation.get('state','') == concept_str]
+def filter_by_state(in_list_operation: list[dict[str, Any]], concept_str: str = "EXECUTED") -> list[dict[str, Any]]:
+    """функция возвращает новый список по ключу concept_str, по умолчанию в статусе 'EXECUTED'"""
+    return [operation for operation in in_list_operation if operation.get("state", "") == concept_str]
 
 
-def sort_by_date(in_list_operation: list[dict[str, Any]], order_by:bool=True) -> list[dict[str, Any]]:
+def sort_by_date(in_list_operation: list[dict[str, Any]], order_by: bool = True) -> list[dict[str, Any]]:
     """Функция возвращает отсортированный по дате операции, по умолчанию - по убыванию"""
-    return sorted(in_list_operation, key=lambda rec: datetime.fromisoformat(rec['date']), reverse=order_by)
-
-
+    return sorted(in_list_operation, key=lambda rec: datetime.fromisoformat(rec["date"]), reverse=order_by)
